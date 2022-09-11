be82cc86a9f33f6c2cae2a6745aea358412133c6
chidinma <chidinmaobasi43@gmail.com>
2022-09-10 18:24:40 -0700

    fixed check error

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 /**
  * main - program compilation begins from main
- * Description: Program to print all possible combinations of numbers
- with no two numbers same in pairing
+ * Description: Program to print all possible combinations of numbers with no
+ * two numbers same as pairing
  * author: sammykingx
  * Return: 0 means success
  */
 int main(void)
 {
-        int a, b;
+	int a, b;
 
-        for (a = 48; a <=57; a++)
-        {
-                for (b = 49; b <= 57; b++)
-                {
-                        if ( a != b && b > a)
-                        {
-                                putchar(a);
-                                putchar(b);
-                                putchar(',');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (a != b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
