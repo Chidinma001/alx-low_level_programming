6c131e2a37c2cf908582b036ff7078d02e5114de
chidinma <chidinmaobasi43@gmail.com>
2022-09-13 11:17:24 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  main -positive or negative
+ *  return: always 0 (success)
+ */
+int main (void)
+{
+	printf("putchar\n");
+
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ALX-low_level_programming
+ program that prints _putchar, followed by a new line
