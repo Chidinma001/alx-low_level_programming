383c3d38c64f5d947ae7a7cf3a3e8342443997c3
chidinma <chidinmaobasi43@gmail.com>
2022-09-14 08:02:39 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,8 @@
  * main - entry point
  * Return: always 0 (Success)
  */
-
 int main(void)
 {
-  char str[] = "_putchar";
-  int ch;
-  for (ch =0; ch <8; ++ch)
-  {
-	  -putchar(str[ch]);
-  }
-  _putchar('\n');
+	printf("_putchar\n");
  return (0);
 }
