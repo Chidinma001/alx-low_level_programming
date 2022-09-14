a32c94ddaa9073a9e55c4da34d1c362d7a804b9f
chidinma <chidinmaobasi43@gmail.com>
2022-09-14 16:30:28 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-#ifnef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#efine MAIN_H
 
-int_putchar(char);
+#include<stdio.h>
+
+int_putchar(char c); 
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +15,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
+void print_times_table(int n);
 
-#endif
+#endif/*MAIN_H*/
