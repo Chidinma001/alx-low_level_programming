e90718adf3f56adf58436c1ae7c28c7dedee690a
chidinma <chidinmaobasi43@gmail.com>
2022-09-15 11:20:43 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function prints every minute of day of Jack
+ * hrs = hours, mnt = minutes
+ * Return: voi
+ */
+voi jack_bauer(void)
+{
+	int i' j;
+
+	i = 0;
+	while (i <=23)
+	{
+		j = 0;
+		while (j <= 59)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar( ' : ' );
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
