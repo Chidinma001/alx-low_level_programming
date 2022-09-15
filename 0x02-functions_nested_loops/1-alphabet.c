01cf0b0a56ede72ac13917041f49dc22acf07ae5
chidinma <chidinmaobasi43@gmail.com>
2022-09-14 17:12:50 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,10 @@
  */
 void print_alphabet(void)
 {
-	char c;
-
-	for (c = 'a'; c < = 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-	return (0);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
-#efine MAIN_H
+#define MAIN_H
 
-#include<stdio.h>
-
-int_putchar(char c); 
+int _putchar(char c); 
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -17,4 +15,4 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif/*MAIN_H*/
+#endif
