c7175ebcec7c6db4ac80d6ab093659cedc27edc9
chidinma <chidinmaobasi43@gmail.com>
2022-09-16 08:24:04 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ * @a: first parameter
+ * @b: secon parameter
+ * Return:sum of the two parameters
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
