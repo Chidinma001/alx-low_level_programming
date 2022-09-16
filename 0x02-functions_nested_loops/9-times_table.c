7dcca505b5946b025dd503d769a76f8dc108c9fe
chidinma <chidinmaobasi43@gmail.com>
2022-09-16 07:20:16 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - create zero to nine times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0,
+		  while (j <= 9)
+		  {
+			  k = i * j;
+			  if (j == 0)
+			  {
+				  _putchar(k + '0');
+			  }
+			  else
+			  {
+				  if (k < 10)
+				  {
+					  _putchar(',');
+					  _putchar(' ');
+					  _putchar(' ');
+					  _putchar(k + '0');
+				  }
+				  else
+				  {
+					  _putchar(',');
+					  _putchar(' ');
+					  _putchar((k / 10) + '0');
+					  _putchar((k % 10) + '0');
+				  }
+			  }
+			  j++;
+		  }
+		_putchar('\n');
+		i++;
+	}
+}
