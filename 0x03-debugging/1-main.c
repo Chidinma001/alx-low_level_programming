0edcd5a675a7b35295ecd3dd369a168e983d143a
chidinma <chidinmaobasi43@gmail.com>
2022-09-16 14:05:46 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,17 +2,16 @@
 
 /**
  * main - causes an infinite loop
- *
- * * Return: 0
+ * Return: 0
  */
-int main(void)
 
+int main(void)
 {
 int i;
 
 printf("infinite loop incoming :(\n");
 
-i = 0,
+i = 0;
 
 /*while (i < 10)*/
 /*{*/
@@ -20,5 +19,6 @@ i = 0,
 /*}*/
 
 printf(infinite loop avoided !\\o / \n");
+
 return (0);
 }
