ad58aa55bfc4235f536c2e3a8f839c69330f1ad4
chidinma <chidinmaobasi43@gmail.com>
2022-09-16 07:51:15 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,33 +12,33 @@ void times_table(void)
 	while (i <= 9)
 	{
 		j = 0;
-		  while (j <= 9)
-		  {
-			  k = i * j;
-			  if (j == 0)
-			  {
-				  _putchar(k + '0');
-			  }
-			  else
-			  {
-				  if (k < 10)
-				  {
-					  _putchar(',');
-					  _putchar(' ');
-					  _putchar(' ');
-					  _putchar(k + '0');
-				  }
-				  else
-				  {
-					  _putchar(',');
-					  _putchar(' ');
-					  _putchar((k / 10) + '0');
-					  _putchar((k % 10) + '0');
-				  }
-			  }
-			  j++;
-		  }
-		_putchar('\n');
-		i++;
-	}
+while (j <= 9)
+{
+k = i * j;
+if (j == 0)
+{
+_putchar(k + '0');
+}
+else
+
+if (k < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
 }
