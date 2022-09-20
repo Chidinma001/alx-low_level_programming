bf676927477afbe1ca43b268daa158a1e0e40500
chidinma <chidinmaobasi43@gmail.com>
2022-09-20 11:55:53 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - function prints string in reverse orer
+ * @s: param for pointer
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
