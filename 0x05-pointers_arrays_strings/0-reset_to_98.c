0fbcbbf8e2804182e80bcd8c779cd2009a984118
chidinma <chidinmaobasi43@gmail.com>
2022-09-19 20:47:51 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+inclue <stdio.h>
+/**
+ * reset_to_98 - reset a pointer
+ * @n: is the input
+ * Return: n
+ * /
+  * void reset_to_98(int *n)
+  {
+  *n = 98;
+  }
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifnef _main_h_
+#efine _main_h_
+
+void reset_t0_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int _putchar(char);
+char *_strcpy(char *dest, char *src);
+#endif
