abd25b7c6bcf76b9553747d3f47fe7bdcd7b00b7
chidinma <chidinmaobasi43@gmail.com>
2022-09-20 10:57:32 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - function for counting length of a string
+ * @s: a pointer for counting the strting
+ * Return: int i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+
+	{
+		i++;
+	}
+
+	return (i);
+}
