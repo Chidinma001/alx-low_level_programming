c442b06856cd67efbd42a1a79a5ec66a2a778fdf
chidinma <chidinmaobasi43@gmail.com>
2022-09-21 11:54:39 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string.c - function that reverses a string
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i, size, half;
+	char first, last;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	size = i - 1;
+	half = size / 2;
+	while (half >= 0)
+	{
+		first = s[size - half];
+		last = s[half];
+		s[half] = first;
+		s[size - half] = last;
+		half--;
+	}
+}
