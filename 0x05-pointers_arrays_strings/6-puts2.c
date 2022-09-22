e37de455ff7ab23e72546116a2bcfac7d8565d25
chidinma <chidinmaobasi43@gmail.com>
2022-09-21 18:12:20 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other string
+ * @str: string input
+ * Return: numbers
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+{
+	if (i % 2 == 0)
+		_putchar(*(str + i));
+	i++;
+}
+_putchar('\n');
+}
