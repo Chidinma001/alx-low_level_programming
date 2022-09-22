22281793cd74ef247f6774eb95309704874f81ae
chidinma <chidinmaobasi43@gmail.com>
2022-09-22 05:19:57 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/9strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * strcpy - copies a string pointed to src an dest
+ * @est: pointer param
+ * @src: pointer param
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = ); *(src 
