c8288509c3f5338f5d5cbe5f1f7deb5570c2e10f
chidinma <chidinmaobasi43@gmail.com>
2022-09-23 19:44:43 -0700

    rectified errors on task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int length, j; /*will hold the lengthof the source */
+	int length, j; /*will hold the length of the source */
 
-		length = 0;
+	length = 0;
 	while (dest[length] != '\0')
-	{
-	length++;
-	}
-for (j = 0; src[j] != '\0'; j++, length++)
-{
-	dest[length] = src[j];
-}
-dest[length] = '\0';
-return (dest);
+		length++;
+
+	for (j = 0; src[j] != '\0'; j++, length++)
+		dest[length] = src[j];
+
+	dest[length] = '\0';
+	return (dest);
 }
