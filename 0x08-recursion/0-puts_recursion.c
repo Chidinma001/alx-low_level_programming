46cc75d5cd30f252c9852f6c7886e874ad2fbf85
chidinma <chidinmaobasi43@gmail.com>
2022-09-29 21:27:02 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - prints the string s with recursion joor
+ * @s: this the variable pointing to the string
+ * Return: returns nothing based on the task
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+
+
+}
+else
+{
+	_putchar('\n');
+}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Testing out my knowledge on recursions loops.
