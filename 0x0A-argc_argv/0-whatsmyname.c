577ce79768a313a7f96c65e4ae545559ae696fdd
chidinma <chidinmaobasi43@gmail.com>
2022-10-04 15:40:24 -0700

    argument to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * @argc: one arguments
+ * @argv: the arguments size
+ * Return: return nothing at this point
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[0]);
+	return (0);
+}
