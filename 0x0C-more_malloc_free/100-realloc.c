c80e7e855a29a0c8870a22d30a8fdb88b9d41476
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2022-10-09 18:19:16 +0100

    Update 100-realloc.c
    a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,29 +18,30 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		clone = ptr;
 	}
 	else
-return (malloc(new_size));
-}
-if (new_size == old_size)
-	return (ptr);
+	{
+		return (malloc(new_size));
 	}
-if (new_size == 0 && ptr != NULL)
-{
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+
+		return (0);
+	}
+	relloc = malloc(new_size);
+
+	if (relloc == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(relloc + i) = clone[i];
+	}
 	free(ptr);
 
-	return (0);
+	return (relloc);
 }
-relloc = malloc(new_size);
-{
-	if (relloc == NULL)
-}
-return (0);
-{
-for (i = 0; i < (old_size || i < new_size); i++)
-*(relloc + i) = clone[i];
-}
-free(ptr);
-
-return (relloc);
-}
-
-
