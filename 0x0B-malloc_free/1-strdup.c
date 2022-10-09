d90a6695a427168951b030179638f3074b9f2124
chidinma <chidinmaobasi43@gmail.com>
2022-10-09 09:16:08 -0700

    fixed task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,15 +7,15 @@
 
 int _strlen(char *s)
 {
-unsigned int i;
+	unsigned int i;
 
-i = 0;
-while (s[i] != '\0') /*count character of string*/
-{
-i++;
-}
+	i = 0;
+	while (s[i] != '\0') /*count character of string*/
+	{
+		i++;
+	}
 
-return (i);
+	return (i);
 }
 
 /**
@@ -27,43 +27,43 @@ return (i);
 
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
+	int i = 0;
 
-while (src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-}
-dest[i] = '\0';
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 
-return (dest);
+	return (dest);
 }
 
 /**
-  *_strdup - array for prints a string
-  *@str: array of elements
-  *Return: pointer
-  */
+ *_strdup - array for prints a string
+ *@str: array of elements
+ *Return: pointer
+ */
 
 char *_strdup(char *str)
 {
-char *dst;
-unsigned int size;
+	char *dst;
+	unsigned int size;
 
-if (str == 0)
-{
-return (NULL);
-}
+	if (str == 0)
+	{
+		return (NULL);
+	}
 
-size = _strlen(str) + 1;
+	size = _strlen(str) + 1;
 
-dst = (char *) malloc(size  *sizeof(char));
+	dst = (char *) malloc(size * sizeof(char));
 
-if (dst == 0)
-{
-	return (NULL);
-}
+	if (dst == 0)
+	{
+		return (NULL);
+	}
 
-_strcpy(dst, str);
-return (dst);
+	_strcpy(dst, str);
+	return (dst);
 }
