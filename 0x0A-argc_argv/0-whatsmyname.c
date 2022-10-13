a7e53b7e8e43062de185d72cd7950db27842bd0f
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2022-10-13 10:43:55 +0100

    Update 0-whatsmyname.c
    a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * main - this is the main function
- * @argc: one arguments
- * @argv: the arguments size
- * Return: return nothing at this point
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	argc = 0;
-
-	printf("%s", argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
