c3596a0efb9c888fd1a259a775f8fca7e456dd9a
chidinma <chidinmaobasi43@gmail.com>
2022-10-26 02:56:30 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 /**
  * main - Print the name of the program
  * @argc: Count arguments
- * @argv: Arguments
+ * @argv: Argument vector
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	
+	//argc = 0;
 	printf("%s\n", argv[0]);
 	return (0);
 }
