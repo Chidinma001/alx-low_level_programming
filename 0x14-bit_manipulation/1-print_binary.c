fd770dc3ee929de9f6f1c16cb2febd29548dd05a
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2022-11-05 13:00:46 +0100

    Create 1-print_binary.c
    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: a string of 0 and 1 chars
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int printbin = ((n & 1) + '0');
+
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar(printbin);
+}
