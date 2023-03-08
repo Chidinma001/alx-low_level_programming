142799374124701586ee5d1157c7abcba79a2c95
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2023-03-08 14:58:40 +0100

    Create 100-funcs.c

---
+++ 0x18-dynamic_libraries/100-funcs.c
@@ -0,0 +1,44 @@
+/**
+ * add - func
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - func
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - func
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - func
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - func
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
